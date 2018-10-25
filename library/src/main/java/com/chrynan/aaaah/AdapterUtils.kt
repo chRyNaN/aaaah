315ead62ec4b88ac2e536f716d7132ed43ac9697
Chris <ckeenan@starry.com>
2018-10-25 10:23:41 -0400

    Added utility functions for creating AnotherAdapters and ManagerRecyclerViewAdapters without explicitly creating classes

---
+++ library/src/main/java/com/chrynan/aaaah/AdapterUtils.kt
@@ -0,0 +1,24 @@
+@file:Suppress("unused")
+
+package com.chrynan.aaaah
+
+import android.view.View
+import android.view.ViewGroup
+
+inline fun <reified M : Any> anotherAdapter(viewType: ViewType,
+                                            crossinline onCreateView: (parent: ViewGroup, viewType: ViewType) -> View,
+                                            crossinline onHandlesItem: (item: M) -> Boolean = { true },
+                                            crossinline onBindItem: (view: View, item: M) -> Unit): AnotherAdapter<M> =
+        object : AnotherAdapter<M>() {
+
+            override val viewType = viewType
+
+            override fun onHandlesItem(item: M) = onHandlesItem(item)
+
+            override fun onCreateView(parent: ViewGroup, viewType: ViewType) = onCreateView(parent, viewType)
+
+            override fun onBindItem(view: View, item: M) = onBindItem(view, item)
+        }
+
+inline fun <reified M : Any> anotherAdapterManager(vararg adapters: AnotherAdapter<*>) =
+        ManagerRecyclerViewAdapter<M>(adapters = adapters.toSet())
