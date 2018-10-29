87ac31458d7ba0c0f7fb08889369ccb5ca8c896c
Chris <ckeenan@starry.com>
2018-10-29 16:10:39 -0400

    Fingers Crossed

--- compiler/src/main/java/com.chrynan.aaaah/builder/KotlinAdapterViewTypeExtensionBuilder.kt
+++ compiler/src/main/java/com.chrynan.aaaah/builder/KotlinAdapterViewTypeExtensionBuilder.kt
@@ -10,16 +10,18 @@ class KotlinAdapterViewTypeExtensionBuilder : KotlinFileSpecBuilder {
     private val anotherAdapterClassName: ClassName by lazy { ClassName(packageName = Packages.AAAAH, simpleName = Names.ANOTHER_ADAPTER) }
     private val genericAnotherAdapterClassName: ParameterizedTypeName by lazy { anotherAdapterClassName.parameterizedBy(WildcardTypeName.STAR) }
     private val javaClassName: ClassName by lazy { ClassName(packageName = Packages.JAVA_CLASS, simpleName = Names.JAVA_CLASS) }
-    private val anotherAdapterGenericJavaClassName: ParameterizedTypeName by lazy { javaClassName.parameterizedBy(genericAnotherAdapterClassName) }
     private val adapterViewTypeSingularClassName: ClassName by lazy { ClassName(packageName = Packages.AAAAH, simpleName = Names.ADAPTER_VIEW_TYPE) }
     private val adapterViewTypesClassName: ClassName by lazy { ClassName(packageName = Packages.AAAAH, simpleName = Names.ADAPTER_VIEW_TYPES) }
     private val viewTypeClassName: ClassName by lazy { ClassName(packageName = Packages.AAAAH, simpleName = Names.VIEW_TYPE) }
+    private val typeVariableName: TypeVariableName by lazy { TypeVariableName("T").withBounds(genericAnotherAdapterClassName) }
+    private val anotherAdapterGenericJavaClassName: ParameterizedTypeName by lazy { javaClassName.parameterizedBy(typeVariableName) }
 
     override fun build(): FileSpec =
             FileSpec.builder(packageName = Packages.AAAAH, fileName = Names.ADAPTER_VIEW_TYPE_EXTENSION)
                     .addFunction(FunSpec.builder("from")
                             .receiver(adapterViewTypeSingularClassName)
                             .returns(viewTypeClassName)
+                            .addTypeVariable(typeVariableName)
                             .addParameter(name = "clazz", type = anotherAdapterGenericJavaClassName)
                             .addStatement("return %T.getInstance().viewTypes[clazz] ?: -1", adapterViewTypesClassName)
                             .build())
