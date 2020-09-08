3c41fccdf5c4126b2b7e2b3f0872266685584f98
Chris <ckeenan@starry.com>
2020-09-08 16:08:09 -0400

    Created buildSrc module

---
+++ buildSrc/src/main/java/com.chrynan.aaaah.buildSrc/LibraryConstants.kt
@@ -0,0 +1,17 @@
+package com.chrynan.aaaah.buildSrc
+
+object LibraryConstants {
+
+    const val bintrayRepo = "chrynan"
+    const val group = "com.chrynan.aaaah"
+    const val owner = "chrynan"
+    const val repoName = "aaaah"
+    const val versionName = "0.7.0"
+    const val versionCode = 2
+    const val versionDescription = "Pre-release 0.7.0"
+    const val license = "Apache-2.0"
+    const val vcsUrl = "https://github.com/chRyNaN/aaaah.git"
+    const val androidCompileSdkVersion = 29
+    const val androidMinSdkVersion = 23
+    const val androidTargetSdkVersion = 29
+}
